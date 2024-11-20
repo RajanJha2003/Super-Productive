ae3bbea2d496aafdcd7da949812fa16cc9d08946
Rajan <rajanj448@student.sfit.ac.in>
2024-11-20 14:02:05 +0530

    intl config

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useTranslations} from 'next-intl';
+
+const Home = () => {
+  
+    const t=useTranslations("Index");
+    return (
+        <h1>
+            {t("title")}
+
+        </h1>
+  )
+}
+
+export default Home
