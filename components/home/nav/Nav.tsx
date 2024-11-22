574b1df64e8bb0c218189256cb3335f820d95a0b
Rajan <rajanj448@student.sfit.ac.in>
2024-11-22 22:01:35 +0530

    mobile navbar

--- components/home/nav/Nav.tsx
+++ components/home/nav/Nav.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import LargeNav from './LargeNav'
+import MobileNav from './MobileNav'
 
 const Nav = () => {
   return (
     <nav className='sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 '>
        <LargeNav />
+       <MobileNav />
     </nav>
   )
 }
