0503f238673e4a9b58549c0e40c8896580b27f9a
Rajan <rajanj448@student.sfit.ac.in>
2024-11-23 22:10:06 +0530

    Created Workspaces

--- components/common/CreatedWorkspacesInfo.tsx
+++ components/common/CreatedWorkspacesInfo.tsx
@@ -1,8 +1,36 @@
+import { MAX_USER_WORKSPACES } from '@/lib/options';
+import { cn } from '@/lib/utils';
+import { useTranslations } from 'next-intl';
 import React from 'react'
 
-const CreatedWorkspacesInfo = () => {
-  return (
-    <div>CreatedWorkspacesInfo</div>
+interface Props{
+    createNumber:number;
+    className?:string;
+}
+
+const CreatedWorkspacesInfo = ({createNumber,className}:Props) => {
+  const t=useTranslations("COMMON");
+  
+    return (
+    <p className={cn("text-muted-foreground sm:text-sm text-xs tect-center",className)}>
+        {
+            t("ACTIVE_WORKSPACES.FIRST")
+        } {" "}
+
+        <span className='font-bold'>
+            {createNumber} {
+                t("ACTIVE_WORKSPACES.SECOND")
+            } {
+                MAX_USER_WORKSPACES
+
+            }
+
+        </span> {" "}
+        {
+            t("ACTIVE_WORKSPACES.THIRD")
+        }
+
+    </p>
   )
 }
 
