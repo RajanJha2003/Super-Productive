988b499f65c09be752bf05ae8f3f5e4befacf534
Rajan <rajanj448@student.sfit.ac.in>
2024-11-28 19:34:37 +0530

    workspace options

--- components/sidebar/optionsSidebar/workspaceOptions/WorkspaceOptions.tsx
+++ components/sidebar/optionsSidebar/workspaceOptions/WorkspaceOptions.tsx
@@ -1,12 +1,33 @@
 "use client"
+import { WorkspaceShortcuts } from '@/types/extended'
+import { useQuery } from '@tanstack/react-query'
 import { useTranslations } from 'next-intl'
 import React from 'react'
+import {WorkspaceOption} from './WorkspaceOption'
+import { PencilRuler } from 'lucide-react'
 
 interface Props{
     workspaceId:string
 }
 
 const WorkspaceOptions = ({workspaceId}:Props) => {
+
+
+
+
+  const { data: workspaceShortcuts, isLoading } = useQuery({
+    queryFn: async () => {
+      const res = await fetch(
+        `/api/workspace/get/workspace_shortcuts?workspaceId=${workspaceId}`
+      );
+
+      if (!res.ok) return null;
+
+      const data = await res.json();
+      return data as WorkspaceShortcuts;
+    },
+    queryKey: ["getWrokspaceShortcuts", workspaceId],
+  });
 
   const t=useTranslations("SIDEBAR.WORKSPACE_OPTIONS")
   return (
@@ -18,7 +39,16 @@ const WorkspaceOptions = ({workspaceId}:Props) => {
       }
     </p>
     {
-      
+      !isLoading && workspaceShortcuts && (
+        <div>
+          <WorkspaceOption>
+            <PencilRuler size={16} />
+            {
+              t("TASKS")
+            }
+          </WorkspaceOption>
+        </div>
+      )
     }
     </div>
 
