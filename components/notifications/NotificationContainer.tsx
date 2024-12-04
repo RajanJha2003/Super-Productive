00375fd06be2a56fc67dead7d96097ea368490c8
Rajan <rajanj448@student.sfit.ac.in>
2024-12-04 23:22:24 +0530

    Client Error

--- components/notifications/NotificationContainer.tsx
+++ components/notifications/NotificationContainer.tsx
@@ -7,6 +7,10 @@ import { Button } from '../ui/button';
 import { Bell } from 'lucide-react';
 import { Notification } from '@prisma/client';
 import { useTranslations } from 'next-intl';
+import { useQuery } from '@tanstack/react-query';
+import { UserNotification } from '@/types/extended';
+import { ClientError } from '../error/ClientError';
+import { LoadingState } from '../ui/loadingState';
 interface Props{
   userId: string;
 }
@@ -16,6 +20,26 @@ const NotificationContainer = ({userId}: Props) => {
     const t = useTranslations("NOTIFICATIONS");
 
     const [unseenNotifications, setUnseenNotifications] = useState<Notification[]>([]);
+
+
+    const {data:UserNotifications,isError,isLoading,refetch}=useQuery<UserNotification[],Error>({
+      queryFn:async()=>{
+        const res=await fetch(`/api/notifications/get?userId=${userId}`);
+
+        if(!res.ok){
+          const error=(await res.json()) as string;
+          throw new Error(error);
+        }
+
+        const response=await res.json();
+        return response
+
+      },
+      refetchInterval:6000,
+      gcTime:1*60*1000,
+      queryKey:["getUserNotifications"]
+
+    })
 
 
 
@@ -52,7 +76,19 @@ const NotificationContainer = ({userId}: Props) => {
           </span>
         </HoverCardContent>
         <PopoverContent side='bottom' align='end' className='w-fit max-w-[300px] sm:max-w-[390px] p-2 sm:p-4'>
+                  {
+                    isError ?(
+                      <ClientError message={t("ERROR")} onRefetch={refetch} className='bg-popover mt-0 sm:mt-0 md:mt-0'
+                       />
+                    ):isLoading ?(
+                      <div className='w-28 h-28 flex justify-center items-center'>
+                        <LoadingState className='w-6 h-6' />
 
+                      </div>
+
+                    ):null
+
+                  }
          
         </PopoverContent>
 
