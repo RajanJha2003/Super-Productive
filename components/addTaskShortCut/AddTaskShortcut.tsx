e04b4aa3e997f7c7500ecd96dfee3782ef45c593
Rajan <rajanj448@student.sfit.ac.in>
2024-12-05 22:32:11 +0530

    emoji done

--- components/addTaskShortCut/AddTaskShortcut.tsx
+++ components/addTaskShortCut/AddTaskShortcut.tsx
@@ -10,6 +10,15 @@ import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import { DateRange } from "react-day-picker";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '../ui/hover-card';
+import { Button } from '../ui/button';
+import { ChevronLeft, ExternalLink, PencilRuler } from 'lucide-react';
+import Link from 'next/link';
+import { ClientError } from '../error/ClientError';
+import { LoadingState } from '../ui/loadingState';
+import MainTab from './MainTab';
+import Workspaces from './Workspaces';
 
 
 interface Props{
@@ -137,7 +146,152 @@ const AddTaskShortcut = ({userId}:Props) => {
   
 
   return (
-    <div>AddTaskShortcut</div>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <HoverCard openDelay={250} closeDelay={250}>
+        <HoverCardTrigger asChild>
+          <DialogTrigger asChild>
+            <Button size="icon" variant="ghost" className='w-8 h-8 sm:w-9 sm:h-9' >
+              <PencilRuler size={18} />
+
+            </Button>
+
+          </DialogTrigger>
+        </HoverCardTrigger>
+        <HoverCardContent align='center'>
+          <span>
+            {
+              t("HINT")
+            }
+          </span>
+
+        </HoverCardContent>
+
+      </HoverCard>
+      <DialogContent className='sm:max-w-[600px]'>
+        <DialogHeader>
+          <div className='flex flex-col items-start gap-2'>
+            {
+              newTaskLink && (
+                <Link target='_blank' href={newTaskLink} className='w-full cursor-pointer' onClick={()=>{
+                  setOpen(false)
+                }}>
+                  <div className='mt-6 mb-4 p-2 border border-primary rounded-md bg-primary/10 w-ful text-primary font-semibold flex justify-between items-center'>
+                  <p>
+                    {
+                      t("ADDED_TASK")
+                    }
+                  </p>
+                  <ExternalLink />
+
+                  </div>
+                </Link>
+              )
+            }
+            <div className='flex items-center gap-2'>
+              {
+                currentTab==="workspaces" && (
+                  <Button onClick={()=>{
+                    changeTabHandler("main")
+                  }} className='h-8 w-8' variant={"ghost"} size={"icon"}>
+                    <ChevronLeft />
+
+                  </Button>
+                )
+              }
+
+
+              <DialogTitle>
+                {
+                  currentTab === "main" ? t("TITLE") : t("CHOOSE_WORKSPACE")
+                }
+              </DialogTitle>
+            </div>
+
+          </div>
+          {
+            currentTab==="main" && (
+              <DialogDescription className='text-left'>
+                {
+                  t("DESC")
+                }
+              </DialogDescription>
+
+            )
+          }
+        </DialogHeader>
+        {
+          isError ? (
+            <ClientError message='Error fetching workspaces' onRefetch={refetch} className='mt-0 sm:mt-0 md:mt-0' />
+
+
+          ):(
+            <>
+            {
+              isGettingWorkspaces ? (
+                <div className='w-full h-20 flex justify-center items-center'>
+                  <LoadingState className='w-10 h-10' />
+
+                </div>
+
+
+              ):(
+                <>
+                <div className='flex flex-col w-full my-4 gap-6'>
+                  {
+                    currentTab==="main" ? (
+                      <MainTab
+                      date={date}
+                      title={title}
+                      renderedEmoji={renderedEmoji}
+                      activeWorkspace={activeWorkspace}
+                      onChangeTitle={changeTitleHandler}
+                      onSelectedDate={selectedDateHandler}
+                      onChangeTabHandler={changeTabHandler}
+                      onSelectEmojiHandler={selectEmojiHandler}
+                    />
+                    ):(
+                      <Workspaces
+                      workspaces={workspaces}
+                      onSelectActiveWorkspace={onSelectActiveWorkspace}
+                    />
+                    )
+                  }
+
+                </div>
+                {currentTab === "main" && (
+                  <DialogFooter className="w-full">
+                    {activeWorkspace ? (
+                      <Button
+                        size={"lg"}
+                        onClick={() => newShortTask()}
+                        disabled={
+                          !activeWorkspace || title.length === 0 || isPending
+                        }
+                        className="w-full text-white"
+                      >
+                        {isPending ? (
+                          <LoadingState loadingText={t("BTN_PENDING")} />
+                        ) : (
+                          t("BTN_ADD")
+                        )}
+                      </Button>
+                    ) : (
+                      <Button
+                        size={"lg"}
+                        onClick={() => setOpen(false)}
+                        className="w-full text-white"
+                      >
+                        {t("BTN_NO_WORKSPACES")}
+                      </Button>
+                    )}
+                  </DialogFooter>
+                )}
+              </>
+            )}
+          </>
+        )}
+      </DialogContent>
+    </Dialog>
   )
 }
 
