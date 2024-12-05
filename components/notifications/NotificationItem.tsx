79f8ba1506a905d00fb3e488be3b044a4db8ee09
Rajan <rajanj448@student.sfit.ac.in>
2024-12-05 08:22:32 +0530

    create notify itwm day

--- components/notifications/NotificationItem.tsx
+++ components/notifications/NotificationItem.tsx
@@ -1,6 +1,39 @@
+"use client"
+
+import { useToast } from '@/hooks/use-toast'
+import { useTruncateText } from '@/hooks/useTrancateText'
+import { UserNotification } from '@/types/extended'
+import { useQueryClient } from '@tanstack/react-query'
+import { useFormatter, useTranslations } from 'next-intl'
 import React from 'react'
 
-const NotificationItem = () => {
+
+interface Props{
+  notify:UserNotification
+}
+
+const NotificationItem = ({notify:{
+    notifyCreator:{image,username},
+    clicked,
+    createdDate,
+    workspace,
+    newUserRole,
+    taskId,
+    mindMapId,
+    notifyType,
+    id
+}}:Props) => {
+    const name=useTruncateText(username,20);
+    const m=useTranslations("MESSAGES");
+    const queryClient=useQueryClient();
+    const format=useFormatter();
+
+    const datTime=new Date(createdDate);
+    const now=new Date();
+
+    const {toast}=useToast();
+    
+
   return (
     <div>NotificationItem</div>
   )
