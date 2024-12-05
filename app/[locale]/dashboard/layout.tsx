bd02baf74dacf4a2ed56eab1ad5fcc2e6264fa7f
Rajan <rajanj448@student.sfit.ac.in>
2024-12-05 17:55:51 +0530

    get editable workspace

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,10 +1,12 @@
 import Sidebar from '@/components/sidebar/Sidebar'
 import { ToggleSidebarProvider } from '@/context/ToggleSidebar'
+import { UserEditableWorkspacesProvider } from '@/context/UserEditableWorkspaces'
 import React from 'react'
 
 const layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <ToggleSidebarProvider>
+   <UserEditableWorkspacesProvider>
+     <ToggleSidebarProvider>
       <div className="flex h-0 min-h-screen w-full overflow-hidden">
             <Sidebar />
             <div className="relative p-4 md:p-6 lg:px-10 flex-grow flex flex-col overflow-y-auto scrollbar-thin scrollbar-thumb-secondary scrollbar-track-background">
@@ -12,6 +14,7 @@ const layout = ({children}: {children: React.ReactNode}) => {
             </div>
           </div>
     </ToggleSidebarProvider>
+   </UserEditableWorkspacesProvider>
   )
 }
 
