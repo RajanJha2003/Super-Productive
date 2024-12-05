9431ab867f5331db494624cd9f207b43435a9484
Rajan <rajanj448@student.sfit.ac.in>
2024-12-05 08:08:24 +0530

    notification contaner

--- components/notifications/NotificationContainer.tsx
+++ components/notifications/NotificationContainer.tsx
@@ -1,16 +1,20 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover';
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '../ui/hover-card';
 import { Button } from '../ui/button';
 import { Bell } from 'lucide-react';
 import { Notification } from '@prisma/client';
 import { useTranslations } from 'next-intl';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { UserNotification } from '@/types/extended';
 import { ClientError } from '../error/ClientError';
 import { LoadingState } from '../ui/loadingState';
+import axios, { AxiosError } from 'axios';
+import { useToast } from '@/hooks/use-toast';
+import { ScrollArea } from '../ui/scroll-area';
+import NotificationItem from './NotificationItem';
 interface Props{
   userId: string;
 }
@@ -18,8 +22,14 @@ interface Props{
 const NotificationContainer = ({userId}: Props) => {
     const [open, setOpen] = useState(false);
     const t = useTranslations("NOTIFICATIONS");
+    const queryClient=useQueryClient();
+    const {toast}=useToast();
+
+    const m=useTranslations("MESSAGES");
 
     const [unseenNotifications, setUnseenNotifications] = useState<Notification[]>([]);
+
+    const [isAnyClickedFalse,setIsAnyClickedFalse]=useState(false);
 
 
     const {data:UserNotifications,isError,isLoading,refetch}=useQuery<UserNotification[],Error>({
@@ -40,6 +50,102 @@ const NotificationContainer = ({userId}: Props) => {
       queryKey:["getUserNotifications"]
 
     })
+
+
+    const {mutate:updateAlltoClickStatus}=useMutation({
+      mutationFn:async()=>{
+        await axios.post("/api/notifications/set-click/all");
+      },
+      onMutate:async()=>{
+        //@ts-ignore
+        await queryClient.cancelQueries(["getUserNotifications"]);
+        const previousNotifications=UserNotifications;
+        const checkedPreviousNotifications=previousNotifications && previousNotifications.length>0 ? previousNotifications:[];
+
+        const updateNotifications=checkedPreviousNotifications.map((notify)=>{
+          return{
+            ...notify,clicked:true
+          }
+        })
+
+        queryClient.setQueryData(["getUserNotifications"],updateNotifications);
+        return {checkedPreviousNotifications};
+
+       
+      },
+      onError:(err:AxiosError,_,context)=>{
+        queryClient.setQueryData(["getUserNotifications"],context?.checkedPreviousNotifications);
+
+        toast({
+          title:m("ERRORS.CANT_UPDATE_SEEN_NOTIFY"),
+          variant:"destructive"
+
+        })
+      },
+      onSettled:()=>{
+        //@ts-ignore
+        queryClient.invalidateQueries(["getUserNotifications"]);
+      },
+      mutationKey:["updateAlltoClickStatus"]
+
+    })
+
+
+    const {mutate:updateToSeenNotifications}=useMutation({
+      mutationFn:async()=>{
+        await axios.post("/api/notifications/set-seen");
+      },
+      onMutate:async()=>{
+        //@ts-ignore
+        await queryClient.cancelQueries(["getUserNotifications"]);
+        const previousNotifications=UserNotifications;
+        const checkedPreviousNotifications=previousNotifications && previousNotifications.length>0 ? previousNotifications:[];
+
+        const updateNotifications=checkedPreviousNotifications.map((notify)=>{
+          return{
+            ...notify,seen:true
+          }
+        })
+
+        queryClient.setQueryData(["getUserNotifications"],updateNotifications);
+        return {checkedPreviousNotifications};
+
+       
+      },
+      onError:(err:AxiosError,_,context)=>{
+        queryClient.setQueryData(["getUserNotifications"],context?.checkedPreviousNotifications);
+
+        toast({
+          title:m("ERRORS.CANT_UPDATE_SEEN_NOTIFY"),
+          variant:"destructive"
+
+        })
+      },
+      onSettled:()=>{
+        //@ts-ignore
+        queryClient.invalidateQueries(["getUserNotifications"]);
+      },
+      mutationKey:["updateToSeenNotifications"]
+
+    })
+
+
+    useEffect(()=>{
+      if(!UserNotifications) return;
+      const unseen=UserNotifications.filter((notification)=>!notification.seen);
+
+      const isAnyClicked=UserNotifications.some((notification)=>!notification.clicked);
+      setIsAnyClickedFalse(isAnyClicked);
+      setUnseenNotifications(unseen);
+      
+    },[UserNotifications])
+
+
+    useEffect(()=>{
+      if(unseenNotifications.length===0) return;
+      updateToSeenNotifications();
+
+    },[open,unseenNotifications,updateToSeenNotifications])
 
 
 
@@ -86,7 +192,45 @@ const NotificationContainer = ({userId}: Props) => {
 
                       </div>
 
-                    ):null
+                    ):UserNotifications && UserNotifications.length>0 ? (
+                      <div className='flex flex-col gap-6'>
+                        <div className='flex gap-2 sm:gap-6 items-center justify-between'>
+                          <h4 className='font-medium leading-none'>
+                            {
+                              t("TITLE")
+                            }
+
+                          </h4>
+                          <Button disabled={!isAnyClickedFalse} onCanPlay={()=>{
+                            updateAlltoClickStatus()
+                          }} className='text-xs' size={"sm"} variant={"secondary"}>
+                            {
+                              t("MARK_AS_READ")
+                            }
+                          </Button>
+
+                        </div>
+                        <ScrollArea className={`${UserNotifications.length>=4 ? UserNotifications.length>=8?"h-96":"h-72":"h-56"}`}>
+                          <div className='flex flex-col gap-3 h-full'>
+                            {
+                              UserNotifications?.map((notify)=>(
+                                <NotificationItem key={notify.id} notify={notify} />
+
+                              ))
+                            }
+
+                          </div>
+
+                        </ScrollArea>
+
+                      </div>
+
+                    ):(
+                      <div className='py-2'>
+                        <p className='font-semibold'>{t("NO_NOTIFICATIONS")}</p>
+
+                      </div>
+                    )
 
                   }
          
