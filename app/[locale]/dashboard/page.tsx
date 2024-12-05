2216418b6ab87e7bece87c0566377bbdcb2913bf
Rajan <rajanj448@student.sfit.ac.in>
2024-12-05 10:22:03 +0530

    dashboard page

--- app/[locale]/dashboard/page.tsx
+++ app/[locale]/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import Welcoming from '@/components/common/Welcoming';
 import DashboardHeader from '@/components/header/DashboardHeader';
 import { getInitialHomeRecentActivity } from '@/lib/api';
 import { checkIfUserCompletedOnboarding } from '@/lib/checkIfUserCompletedOnboarding';
@@ -13,6 +14,9 @@ const page = async() => {
   return (
     <>
     <DashboardHeader />
+    <Welcoming hideOnDesktop
+     className='px-4 py-2'
+      username={session.user.username!} name={session.user.name} surname={session.user.surname}  />
     
     </>
   )
