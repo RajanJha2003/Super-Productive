a3e86893b0b87603f77c9813f95e5e4bdb584046
Rajan <rajanj448@student.sfit.ac.in>
2024-12-06 19:08:36 +0530

    members row

---
+++ components/settings/workspace/members/Row/MembersRow.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { UserAvatar } from '@/components/ui/user-avatar';
+import { SubscriptionUser } from '@/types/extended';
+import { UserPermission as UserPermissionType } from '@prisma/client';
+import React from 'react'
+
+interface Props{
+    userRole:UserPermissionType,
+    user:{
+        id:string,
+        image?:string|null|undefined;
+        username:string
+    },
+    workspaceId:string,
+    onSetworkspacesubscribers: React.Dispatch<
+    React.SetStateAction<SubscriptionUser[]>
+  >;
+}
+
+
+const MembersRow = ({userRole,user,workspaceId,onSetworkspacesubscribers}:Props) => {
+  return (
+    <li className='w-full grid grid-cols-3 items-center grid-rows-1 gap-4 p-4 border-b last:border-b-0 text-sm sm:text-base h-16'>
+
+        <div className='flex items-center gap-2'>
+            <UserAvatar profileImage={user.image} size={14} className='w-8 h-8 hidden sm:flex'  />
+            <p className='font-semibold'>{user.username}</p>
+
+        </div>
+        
+    </li>
+  )
+}
+
+export default MembersRow
