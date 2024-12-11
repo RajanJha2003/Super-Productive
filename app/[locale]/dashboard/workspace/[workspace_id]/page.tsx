6716bd224d600ef42ac455a73718dee447e5de57
Rajan <rajanj448@student.sfit.ac.in>
2024-12-11 14:29:18 +0530

    get user role

--- app/[locale]/dashboard/workspace/[workspace_id]/page.tsx
+++ app/[locale]/dashboard/workspace/[workspace_id]/page.tsx
@@ -1,9 +1,28 @@
 import DashboardHeader from '@/components/header/DashboardHeader'
 import { FilterByUsersAndTagsInWorkspaceProvider } from '@/context/FilterByUsersAndTagsInWorkspace'
+import { gertWorkspaceWithChatId, getUserWorkspaceRole } from '@/lib/api';
+import { checkIfUserCompletedOnboarding } from '@/lib/checkIfUserCompletedOnboarding';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
-const page = () => {
 
+interface Params{
+  params:{
+    workspace_id:string;
+  }
+}
+
+const page = async({params:{workspace_id}}:Params) => {
+
+  const session=await checkIfUserCompletedOnboarding(`/dashboard/workspace/${workspace_id}`);
+
+  const [workspace,userRole]=await Promise.all([
+    gertWorkspaceWithChatId(workspace_id,session.user.id),
+    getUserWorkspaceRole(workspace_id,session.user.id)
+  ])
+   
+
+  if(!workspace || !userRole) notFound();
 
   
   return (
