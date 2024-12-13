5ea5bb703db2dd9c56af9e8db8f1e931270a2dc0
Rajan <rajanj448@student.sfit.ac.in>
2024-12-13 12:04:40 +0530

    assigned to task api

--- components/tasks/assignToTask/AssignedToTaskSelector.tsx
+++ components/tasks/assignToTask/AssignedToTaskSelector.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+"use client";
 
-const AssignedToTaskSelector = () => {
-  return (
+import { useUserEditableWorkspaces } from '@/context/UserEditableWorkspaces';
+import { UsersAssignedToTaskInfo } from '@/types/extended';
+import { useQuery } from '@tanstack/react-query';
+import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+
+interface Props {
+    className?: string;
+    plusIconSize?: number;
+    dropdownSizeOffset?: number;
+    workspaceId: string;
+    taskId: string;
+  }
+
+const AssignedToTaskSelector = ({className,plusIconSize,dropdownSizeOffset,workspaceId,taskId}:Props) => {
+    const t = useTranslations("TASK.ASSIGNMENT");
+    const [canEdit, setCanEdit] = useState(false);
+  
+    const {
+      data: editableWorkspaces,
+      isError: isErrorGettingWorkspaces,
+      isLoading: isGettingWorkspaces,
+      refetch: refetchWorkspaces,
+    } = useUserEditableWorkspaces();
+  
+    useEffect(() => {
+      if (editableWorkspaces) {
+        const inThisWorkspace = editableWorkspaces.some(
+          (workspace) => workspace.id === workspaceId
+        );
+        setCanEdit(inThisWorkspace);
+      }
+    }, [editableWorkspaces, workspaceId]);
+    const {
+      data: assignedUsersInfo,
+      isLoading: isLoadingInfo,
+      isError: isErrorGettingAssignedUser,
+      refetch: refetchAssigned,
+    } = useQuery({
+      queryFn: async () => {
+        const res = await fetch(
+          `/api/assigned_to/tasks/get?workspaceId=${workspaceId}&taskId=${taskId}`
+        );
+  
+        if (!res.ok) return {} as UsersAssignedToTaskInfo;
+  
+        const data = await res.json();
+        return data as UsersAssignedToTaskInfo;
+      },
+  
+      queryKey: ["getAssignedToTaskInfo", taskId],
+    });
+  
+    const router = useRouter();
+ 
+    return (
     <div>AssignedToTaskSelector</div>
   )
 }
--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 declare global {
   var prisma: PrismaClient;
@@ -6,13 +9,33 @@ declare global {
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV !== "production") prisma = new PrismaClient();
-else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
+// Function to initialize Prisma Client
+function initializePrisma() {
+  if (process.env.NODE_ENV !== "production") {
+    prisma = new PrismaClient();
+  } else {
+    if (!global.prisma) {
+      global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
   }
-  prisma = global.prisma;
 }
 
+// Initialize Prisma Client
+initializePrisma();
 
-export const db=prisma;
+// Connect to the database
+async function connectToDatabase() {
+  try {
+    await prisma.$connect();
+    console.log("Connected to the database successfully.");
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+  }
+}
+
+// Call the connect function immediately
+connectToDatabase();
+
+// Export the db instance for use throughout the project
+export const db = prisma;
