a369c3bb0d8c454fcda0cab3496b5db588179300
Rajan <rajanj448@student.sfit.ac.in>
2024-12-13 10:16:23 +0530

    delete task api

--- schema/taskSchema.ts
+++ schema/taskSchema.ts
@@ -1,36 +1,15 @@
 import { z } from "zod";
-import { color } from "./workSpaceSchema";
 
-const tagName = z
-  .string()
-  .min(2, "SCHEMA.WORKSPACE.SHORT")
-  .max(20, "SCHEMA.WORKSPACE.LONG")
-  .refine((username) => /^[a-zA-Z0-9]+$/.test(username), {
-    message: "SCHEMA.WORKSPACE.SPECIAL_CHARS",
-  });
-
-const id = z.string();
-
-export const tagSchema = z.object({
-  id,
-  tagName,
-  color,
+export const taskSchema = z.object({
+  icon: z.string(),
+  title: z.string().optional(),
+  date: z.any(),
+  content: z.any(),
 });
 
-export const apiTagSchema = z.object({
-  id,
-  workspaceId: id,
-  tagName,
-  color,
+export const deleteTaskSchema = z.object({
+  taskId: z.string(),
+  workspaceId: z.string(),
 });
 
-export const apiDeleteTagSchema = z.object({
-  id,
-  workspaceId: id,
-});
-
-export type ApiDeleteTagSchema = z.infer<typeof apiDeleteTagSchema>;
-
-export type ApiTagSchema = z.infer<typeof apiTagSchema>;
-
-export type TagSchema = z.infer<typeof tagSchema>;
+export type TaskSchema = z.infer<typeof taskSchema>;
