c16e6e691bbf5a91fca3b2941bbac46dcdd5cec0
Rajan <rajanj448@student.sfit.ac.in>
2024-12-21 11:01:19 +0530

    use tags hooks

--- components/tasks/editable/container/TaskContainer.tsx
+++ components/tasks/editable/container/TaskContainer.tsx
@@ -1,6 +1,126 @@
-import React from 'react'
+"use client";
 
-const TaskContainer = () => {
+import { useAutosaveIndicator } from '@/context/AutoSaveIndicator';
+import { taskSchema, TaskSchema } from '@/schema/taskSchema';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { Tag } from '@prisma/client';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+import { useTranslations } from 'next-intl';
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { DateRange } from 'react-day-picker';
+import { useForm } from 'react-hook-form';
+import {useDebouncedCallback} from "use-debounce";
+
+
+interface Props{
+   workspaceId:string;
+   initialActiveTags:Tag[];
+   taskId:string;
+   title?:string;
+   content?:JSON;
+   emoji?:string;
+   from?:Date;
+   to?:Date;
+}
+const TaskContainer = ({workspaceId,initialActiveTags,taskId,title,content,emoji,from,to}:Props) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const _titleRef = useRef<HTMLTextAreaElement>(null);
+  const t = useTranslations("TASK");
+  const [taskDate] = useState({ from, to });
+
+  const { status, onSetStatus } = useAutosaveIndicator();
+
+  const form = useForm<TaskSchema>({
+    resolver: zodResolver(taskSchema),
+    defaultValues: {
+      icon: emoji,
+      title: title ? title : "",
+    },
+  });
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const { ref: titleRef, ...rest } = form.register("title");
+
+  const onFormSelectHandler = (emoji: string) => {
+    form.setValue("icon", emoji);
+  };
+
+  const onUpdateFormHandler = (date: DateRange | undefined) => {
+    form.setValue("date", date);
+  };
+
+  const { mutate: updateTaskTitle, isPending } = useMutation({
+    mutationFn: async (title: string) => {
+      await axios.post("/api/task/update/title", {
+        workspaceId,
+        title,
+        taskId,
+      });
+    },
+    onSuccess: () => {
+      onSetStatus("saved");
+    },
+    onError: () => {
+      onSetStatus("unsaved");
+    },
+  });
+
+  const { mutate: updateTaskActiveTags } = useMutation({
+    mutationFn: async (tagIds: string[]) => {
+      await axios.post("/api/task/update/active_tags", {
+        workspaceId,
+        tagsIds: tagIds,
+        taskId,
+      });
+    },
+    onSuccess: () => {
+      onSetStatus("saved");
+    },
+    onError: () => {
+      onSetStatus("unsaved");
+    },
+  });
+
+  const debouncedTitle = useDebouncedCallback(
+    useCallback((value: string) => {
+      onSetStatus("pending");
+      updateTaskTitle(value);
+    }, []),
+    2000
+  );
+
+  const debouncedCurrentActiveTags = useDebouncedCallback(() => {
+    onSetStatus("pending");
+    const tagsIds = currentActiveTags.map((tag) => tag.id);
+    updateTaskActiveTags(tagsIds);
+  }, 2000);
+
+  const onSubmit = (data: TaskSchema) => {
+    console.log(data);
+  };
+
+  const {
+    currentActiveTags,
+    tags,
+    isError,
+    isLoadingTags,
+    onDeleteActiveTagHandler,
+    onSelectActiveTagHandler,
+    onUpdateActiveTagHandler,
+  } = useTags(
+    workspaceId,
+    isMounted,
+    initialActiveTags,
+    debouncedCurrentActiveTags
+  );
+
+
+  
   return (
     <div>TaskContainer</div>
   )
